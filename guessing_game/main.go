d37b57b0fc3dbaf03a332ffcf86a5c39a2eda1e7
Norman Shipman <norman@nshipman.io>
2020-11-09 11:12:04 -0500

    Worked on go error handling and control flow

--- guessing_game/main.go
+++ guessing_game/main.go
@@ -1,43 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func guessing_game() {
-	var userNum int
 	rand.Seed(time.Now().UnixNano())
+	reader := bufio.NewReader(os.Stdin)
 	randomNum := rand.Intn(100-0) + 0
 	guesses := 3
-	for guesses > 0 {
+	for true {
+		if guesses == 0 {
+			fmt.Printf("You ran out of guesses.\n The number was %d. Try again!", randomNum)
+			os.Exit(0)
+		}
+
 		fmt.Printf("You have %d guesses remaining.\n", guesses)
 		fmt.Print("Guess a number between 0 and 100: ")
-		_, err := fmt.Scanf("%d", &userNum)
+		input, err := reader.ReadString('\n')
 
 		if err != nil {
-			fmt.Println(err)
-		}
-
-		if userNum < randomNum {
-			fmt.Println("Too low")
-			guesses -= 1
-		} else if userNum > randomNum {
-			fmt.Println("Too high")
-			guesses -= 1
+			fmt.Println("An error has occured while reading input.")
 		} else {
-			fmt.Println("Just right")
-			break
+			input = strings.TrimSuffix(input, "\n")
+			userNum, err := strconv.Atoi(input)
+			if err != nil {
+				fmt.Println("Invalid input. Please enter an integer value.")
+			} else {
+				if userNum < randomNum {
+					fmt.Println("Too low")
+					guesses -= 1
+				} else if userNum > randomNum {
+					fmt.Println("Too high")
+					guesses -= 1
+				} else {
+					fmt.Println("Just right")
+					break
+				}
+			}
 		}
-
 	}
 
-	if guesses == 0 {
-		fmt.Printf("You ran out of guesses.\n The number was %d. Try again!", randomNum)
-		os.Exit(0)
-	}
 }
 
 func main() {
